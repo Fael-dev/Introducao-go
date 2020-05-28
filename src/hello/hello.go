6e825ed57e094b5d1fa2cde6f727f4ede63b124a
Rafael <faelleonan@gmail.com>
2020-05-28 14:25:30 -0300

    Conhecendo, switch case

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,31 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 
+	introducao()
+	menu()
+
+	comando := leComando()
+	switch comando {
+	case 1:
+		fmt.Println("\nMonitorando...")
+	case 2:
+		fmt.Println("\nExibindo logs...")
+	case 0:
+		fmt.Println("\nSaindo do programa.")
+		os.Exit(0)
+	default:
+		fmt.Println("\nNão conheço este comando.")
+		os.Exit(-1)
+	}
+}
+
+func introducao() {
 	nome := "Rafael"
 	versao := 1.1
 	fmt.Println("Olá Sr.", nome)
 	fmt.Println("A versão do programa é ", versao)
+}
+
+func leComando() int {
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O comando escolhido foi ", comando)
+
+	return comando
+}
+
+func menu() {
 	fmt.Println("\n1 - Iniciar monitoramento.")
 	fmt.Println("2 - Exibir logs.")
 	fmt.Println("0 - Sair do programa.")
-
-	var comando int
-	fmt.Scan(&comando)
-
-	fmt.Println("O endereço da minha variável comando é ", &comando)
-	fmt.Println("O comando escolhido foi ", comando)
-
-	if comando == 1 {
-		fmt.Println("\nMonitorando...")
-	} else if comando == 2 {
-		fmt.Println("\nExibindo logs...")
-	} else if comando == 0 {
-		fmt.Println("\nSaindo do programa.")
-	} else {
-		fmt.Println("\nNão conheço este comando.")
-	}
-
 }
