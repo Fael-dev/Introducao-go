4012924453e88219a9779653213e3fc1756c971d
Rafael <faelleonan@gmail.com>
2020-05-28 11:43:19 -0300

    Conhecendo fmt.Scan e fmt.Scanf

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world. By developer Rafael!")
+
+	nome := "Rafael"
+	versao := 1.1
+	fmt.Println("Olá Sr.", nome)
+	fmt.Println("A versão do programa é ", versao)
+	fmt.Println("\n1 - Iniciar monitoramento.")
+	fmt.Println("2 - Exibir logs.")
+	fmt.Println("0 - Sair do programa.")
+
+	var comando int
+	fmt.Scan(&comando)
+
+	fmt.Println("O endereço da minha variável comando é ", &comando)
+	fmt.Println("O comando escolhido foi ", comando)
 }
